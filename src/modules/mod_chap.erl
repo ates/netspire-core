841bf32354d42c8f0855eecaa1b149c21e5a0d64
Artem Teslenko <ates@ipv6.dp.ua>
2009-12-20 14:14:07 +0200

    Implementation of CHAP authentication protocol (RFC 1994)

---
+++ src/modules/mod_chap.erl
@@ -0,0 +1,48 @@
+%%% Implementation of CHAP authentication protocol (RFC 1994)
+-module(mod_chap).
+
+-behaviour(gen_module).
+
+%% API
+-export([verify_chap/6]).
+
+%% gen_module callbacks
+-export([start/1, stop/0]).
+
+-include("../netspire.hrl").
+-include("../netspire_radius.hrl").
+-include("../radius/radius.hrl").
+
+start(_Options) ->
+    ?INFO_MSG("Starting dynamic module ~p~n", [?MODULE]),
+    netspire_hooks:add(radius_auth, ?MODULE, verify_chap).
+
+stop() ->
+    ?INFO_MSG("Stop dynamic module ~p~n", [?MODULE]),
+    netspire_hooks:delete(radius_auth, ?MODULE, verify_chap).
+
+verify_chap(_, Request, UserName, Password, Replies, _Client) ->
+    case radius:attribute_value(?CHAP_PASSWORD, Request) of
+        undefined -> undefined;
+        Value ->
+            Challenge =
+                case radius:attribute_value(?CHAP_CHALLENGE, Request) of
+                	undefined -> Request#radius_packet.auth; 
+                    Value ->
+                        Value
+                end,
+            ChapPassword = list_to_binary(Value),
+            do_chap(UserName, ChapPassword, Challenge, Password, Replies)
+    end.
+
+do_chap(UserName, <<ChapId, ChapPassword/binary>>, Challenge, Password, Replies) ->
+    PasswordHash = erlang:md5([ChapId, Password, Challenge]),
+    case PasswordHash == ChapPassword of
+        true ->
+            ?INFO_MSG("CHAP authentication succeeded: ~p~n", [UserName]),
+            {stop, {accept, Replies}};
+        _ ->
+            ?INFO_MSG("CHAP authentication failed: ~p~n", [UserName]),
+            {stop, {reject, []}}
+    end.
+
